3a566e9ba53f899edeb8fc976a727147c5561585
sala-207 <sala-207@10.68.76.210>
2020-03-06 13:56:42 -0300

    Aula 06/03/2020.

---
+++ src/lista/fila/exemplo/model/Funcionario.java
@@ -0,0 +1,62 @@
+package lista.fila.exemplo.model;
+
+public class Funcionario {
+
+	private String nome;
+	private String matricula;
+	private String funcao;
+	private double salario;
+	
+	public Funcionario() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Funcionario(String nome, String matricula, String funcao, double salario) {
+		super();
+		this.nome = nome;
+		this.matricula = matricula;
+		this.funcao = funcao;
+		this.salario = salario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionario \n nome= " + nome + "\n matricula= " + matricula + "\n funcao= " + funcao + "\n salario= " + salario + "\n";
+	}
+	
+	
+	
+	
+}
---
+++ src/lista/fila/exemplo/view/QueueExample.java
@@ -0,0 +1,57 @@
+package lista.fila.exemplo.view;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import lista.fila.exemplo.model.Funcionario;
+
+public class QueueExample {
+
+	public static void main(String[] args) {
+
+		Queue<Funcionario> waitingQueue = new LinkedList<>();
+
+		waitingQueue.add(new Funcionario("Adriano", "38175", "Eletricista", 350.25));
+		waitingQueue.add(new Funcionario("Leandro&Leonardo", "6723", "Carinha do Mouse", 100.00));
+		waitingQueue.add(new Funcionario("Alicia", "111047", "Assistente do Adriano", 2.25));
+
+		
+//		for (Funcionario funcionario : waitingQueue) {
+//			
+//			System.out.println(funcionario);
+//			
+//		}
+		
+		System.out.println(waitingQueue.remove());
+		
+		System.out.println(waitingQueue.peek());
+
+//		System.out.println("Fila vazia: " + waitingQueue.isEmpty());
+//
+//		System.out.println("Tamanho: " + waitingQueue.size());
+//
+//		System.out.println("Procurar elemento: " + waitingQueue.contains("Lucas"));
+//
+//		System.out.println("Elemento da frente: " + waitingQueue.element());
+//
+//		System.out.println(" | Nova Fila: " + waitingQueue);
+//
+//		System.out.println("Tamanho: " + waitingQueue.size());
+
+//	    ArrayList<String> col = new ArrayList<>();
+//	    
+//	    col.add("Lucas");
+//			
+//		
+////	    System.out.println(waitingQueue.peek());
+//	    
+//	    waitingQueue.removeAll(col);
+//	    
+//	    name = waitingQueue.poll();
+//		System.out.println(" | Nova Fila: " + waitingQueue);
+	}
+
+}
